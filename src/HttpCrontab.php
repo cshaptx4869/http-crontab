c3959f2c4629840c20a46973d58d8951b5103cb2
cshaptx4869 <994774638@qq.com>
2023-01-17 14:31:51 +0800

    Mod: 增加禁用函数pcntl_wait检测

--- src/HttpCrontab.php
+++ src/HttpCrontab.php
@@ -148,6 +148,7 @@ class HttpCrontab
                 "pcntl_signal",
                 "pcntl_alarm",
                 "pcntl_fork",
+                "pcntl_wait",
                 "posix_getuid",
                 "posix_getpwuid",
                 "posix_kill",
